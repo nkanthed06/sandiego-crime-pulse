ac34711c1d28754e0408721dfe5ae19a2de4e958
nkanthed06 <niravkanthed5@gmail.com>
2025-12-02 12:36:33 -0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,5 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/sandiego-crime-pulse/',
   server: {
     host: "::",
     port: 8080,
